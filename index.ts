b056b02a62837837d02dd4cd51633efb5e3beeda
Jason L <jluboff@gmail.com>
2019-07-11 15:28:13 -0700

    Add/Get levels. Add/Get log.

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import { ConnectionPool } from 'mssql';
 import { initializeDBConnection } from './controllers/connectSQL';
-import { WriteErrorProps, ConnectSQLProps } from './types/interfaces';
+import {
+  WriteErrorProps,
+  ConnectSQLProps,
+  ErrorReport
+} from './types/interfaces';
 import * as insert from './db/insertQueries';
 import * as read from './db/readQueries';
 
@@ -17,33 +21,6 @@ export default class AppLogger {
     this.database = props.database || 'AppLogger';
   }
 
-  public addApplication = () => {};
-  public retrieveApplicationID = () => {};
-  public addLevel = () => {};
-  public retrieveLevel = () => {};
-  public getErrorLog = async () => {
-    const db = await this.connectToDatabase();
-    const errorLog = await read.errors(db);
-
-    db.close();
-    return errorLog;
-  };
-  public writeError = async (props: WriteErrorProps) => {
-    try {
-      const db = await this.connectToDatabase();
-      await insert.error(db, {
-        applicationID: props.applicationID,
-        levelID: props.levelID,
-        userID: props.userID,
-        errorID: props.errorID,
-        methodID: props.methodID,
-        routeID: props.routeID
-      });
-      db.close();
-    } catch (error) {
-      throw error;
-    }
-  };
   private connectToDatabase = async (): Promise<ConnectionPool> => {
     try {
       const sqlConfig = {
@@ -55,6 +32,62 @@ export default class AppLogger {
       const connect = await initializeDBConnection(sqlConfig);
 
       return connect;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  public addApplication = () => {};
+  public retrieveApplicationID = () => {};
+
+  public addLevel = async (level: string): Promise<number> => {
+    try {
+      const db = await this.connectToDatabase();
+      const newLevelID = await insert.level(db, level);
+
+      db.close();
+      return newLevelID;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  public retrieveLevel = async () => {
+    try {
+      const db = await this.connectToDatabase();
+      const levels = await read.levels(db);
+
+      db.close();
+      return levels;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  public retrieveErrorLog = async (): Promise<Array<ErrorReport>> => {
+    try {
+      const db = await this.connectToDatabase();
+      const errorLog = await read.errors(db);
+
+      db.close();
+      return errorLog;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  public writeError = async (props: WriteErrorProps): Promise<void> => {
+    try {
+      const db = await this.connectToDatabase();
+      await insert.error(db, {
+        applicationID: props.applicationID,
+        levelID: props.levelID,
+        userID: props.userID,
+        errorID: props.errorID,
+        methodID: props.methodID,
+        routeID: props.routeID
+      });
+      db.close();
     } catch (error) {
       throw error;
     }
